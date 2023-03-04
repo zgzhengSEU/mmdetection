eb722862c01441e8949bdd125c82b159425e6d0b
zanezgzheng <zzg.seu@outlook.com>
2023-03-05 01:45:08 +0800

    fsrcnn

--- myconfig/VisDrone-wxg/faster-rcnn/faster-rcnn_r50_fpn_1x_coco.py
+++ myconfig/VisDrone-wxg/faster-rcnn/faster-rcnn_r50_fpn_1x_coco.py
@@ -4,7 +4,10 @@ _base_ = [
     '../../../configs/_base_/schedules/schedule_1x.py', '../../../configs/_base_/default_runtime.py'
 ]
 
-# ======================== wandb & run ==============================
+# ======================== wandb & run =========================================================================================
+fp16 = dict(loss_scale=512.)
+
+# ===========================================
 TAGS = ["r50_fpn_1x"]
 GROUP_NAME = "faster-rcnn"
 ALGO_NAME = "faster-rcnn_r50_fpn_1x_coco"
@@ -21,17 +24,18 @@ Wandb_init_kwargs = dict(
 )
 visualizer = dict(vis_backends = [dict(type='LocalVisBackend'), dict(type='WandbVisBackend', init_kwargs=Wandb_init_kwargs)])
 
+# ==========================================
 import datetime as dt
 NOW_TIME = dt.datetime.now().strftime('%Y%m%d_%H%M%S')
 work_dir = f"work_dirs/{DATASET_NAME}/{ALGO_NAME}/{NOW_TIME}"
 
 load_from = "https://download.openmmlab.com/mmdetection/v2.0/faster_rcnn/faster_rcnn_r50_fpn_1x_coco/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth"
 
-# =============== datasets ===========================================
+# =============== datasets ======================================================================================================
 # Batch size of a single GPU during training
-train_batch_size_per_gpu = 16
+train_batch_size_per_gpu = 4
 # Worker to pre-fetch data for each single GPU during training
-train_num_workers = 8
+train_num_workers = 4
 # Batch size of a single GPU during valing
 val_batch_size_per_gpu = 1
 # Worker to pre-fetch data for each single GPU during valing
--- myconfig/VisDrone-wxg/faster-rcnn/faster-rcnn_r50_fpn_2x_coco.py
+++ myconfig/VisDrone-wxg/faster-rcnn/faster-rcnn_r50_fpn_2x_coco.py
@@ -1,5 +1,54 @@
 _base_ = [
-    '../_base_/models/faster-rcnn_r50_fpn.py',
-    '../_base_/datasets/coco_detection.py',
-    '../_base_/schedules/schedule_2x.py', '../_base_/default_runtime.py'
+    '../../../configs/_base_/models/faster-rcnn_r50_fpn.py',
+    '../../../configs/_base_/datasets/visdrone_detection.py',
+    '../../../configs/_base_/schedules/schedule_2x.py', '../../../configs/_base_/default_runtime.py'
 ]
+
+# ======================== wandb & run =========================================================================================
+fp16 = dict(loss_scale=512.)
+
+# ===========================================
+TAGS = ["FR_r50_fpn_2x"]
+GROUP_NAME = "faster-rcnn"
+ALGO_NAME = "faster-rcnn_r50_fpn_2x_coco"
+DATASET_NAME = "VisDrone"
+
+Wandb_init_kwargs = dict(
+    project=DATASET_NAME,
+    group=GROUP_NAME,
+    name=ALGO_NAME,
+    tags=TAGS,
+    resume="allow",
+    # id="",
+    allow_val_change=True
+)
+visualizer = dict(vis_backends = [dict(type='LocalVisBackend'), dict(type='WandbVisBackend', init_kwargs=Wandb_init_kwargs)])
+
+# ==========================================
+import datetime as dt
+NOW_TIME = dt.datetime.now().strftime('%Y%m%d_%H%M%S')
+work_dir = f"work_dirs/{DATASET_NAME}/{ALGO_NAME}/{NOW_TIME}"
+
+load_from = "https://download.openmmlab.com/mmdetection/v2.0/faster_rcnn/faster_rcnn_r50_fpn_2x_coco/faster_rcnn_r50_fpn_2x_coco_bbox_mAP-0.384_20200504_210434-a5d8aa15.pth"
+
+# =============== datasets ======================================================================================================
+# Batch size of a single GPU during training
+train_batch_size_per_gpu = 4
+# Worker to pre-fetch data for each single GPU during training
+train_num_workers = 4
+# Batch size of a single GPU during valing
+val_batch_size_per_gpu = 1
+# Worker to pre-fetch data for each single GPU during valing
+val_num_workers = 2
+# Batch size of a single GPU during valing
+test_batch_size_per_gpu = 1
+# Worker to pre-fetch data for each single GPU during valing
+test_num_workers = 2
+
+train_dataloader = dict(batch_size=train_batch_size_per_gpu, num_workers=train_num_workers)
+val_dataloader = dict(batch_size=val_batch_size_per_gpu, num_workers=val_num_workers)
+test_dataloader = dict(batch_size=test_batch_size_per_gpu, num_workers=test_num_workers)
+
+model = dict(
+    roi_head=dict(
+        bbox_head=dict(num_classes=10)))
