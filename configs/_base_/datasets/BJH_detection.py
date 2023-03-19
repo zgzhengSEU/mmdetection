4e05d542b8d2df9fd50cdea1fcded5f1c70871c7
zanezgzheng <zzg.seu@outlook.com>
2023-03-19 19:23:51 +0800

    BJH

---
+++ configs/_base_/datasets/BJH_detection.py
@@ -0,0 +1,128 @@
+# dataset settings
+dataset_type = 'CocoDataset'
+data_root = 'data/BJHDrone/'
+CLASSES = ("car", "bus", "van", "rider", "person")
+METAINFO = {'classes': CLASSES}
+# Path of train annotation file
+train_ann_file = 'annotations/train.json'
+train_data_prefix = 'images/train/'  # Prefix of train image path
+# Path of val annotation file
+val_ann_file = 'annotations/test.json'
+val_data_prefix = 'images/test/'  # Prefix of val image path
+# Path of test annotation file
+test_ann_file = 'annotations/test.json'
+test_data_prefix = 'images/test/'  # Prefix of val image path
+
+# Batch size of a single GPU during training
+train_batch_size_per_gpu = 2
+# Worker to pre-fetch data for each single GPU during training
+train_num_workers = 2
+# Batch size of a single GPU during valing
+val_batch_size_per_gpu = 1
+# Worker to pre-fetch data for each single GPU during valing
+val_num_workers = 2
+# Batch size of a single GPU during valing
+test_batch_size_per_gpu = 1
+# Worker to pre-fetch data for each single GPU during valing
+test_num_workers = 2
+
+# file_client_args = dict(
+#     backend='petrel',
+#     path_mapping=dict({
+#         './data/': 's3://openmmlab/datasets/detection/',
+#         'data/': 's3://openmmlab/datasets/detection/'
+#     }))
+file_client_args = dict(backend='disk')
+
+train_pipeline = [
+    dict(type='LoadImageFromFile', file_client_args=file_client_args),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='Resize', scale=(1333, 800), keep_ratio=True),
+    dict(type='RandomFlip', prob=0.5),
+    dict(type='PackDetInputs')
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile', file_client_args=file_client_args),
+    dict(type='Resize', scale=(1333, 800), keep_ratio=True),
+    # If you don't have a gt annotation, delete the pipeline
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(
+        type='PackDetInputs',
+        meta_keys=('img_id', 'img_path', 'ori_shape', 'img_shape',
+                   'scale_factor'))
+]
+train_dataloader = dict(
+    batch_size=train_batch_size_per_gpu,
+    num_workers=train_num_workers,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=True),
+    batch_sampler=dict(type='AspectRatioBatchSampler'),
+    dataset=dict(
+        type=dataset_type,
+        data_root=data_root,
+        metainfo=METAINFO,
+        ann_file=train_ann_file,
+        data_prefix=dict(img=train_data_prefix),
+        filter_cfg=dict(filter_empty_gt=True, min_size=32),
+        pipeline=train_pipeline))
+val_dataloader = dict(
+    batch_size=val_batch_size_per_gpu,
+    num_workers=val_num_workers,
+    persistent_workers=True,
+    drop_last=False,
+    sampler=dict(type='DefaultSampler', shuffle=False),
+    dataset=dict(
+        type=dataset_type,
+        data_root=data_root,
+        metainfo=METAINFO,
+        ann_file=val_ann_file,
+        data_prefix=dict(img=val_data_prefix),
+        test_mode=True,
+        pipeline=test_pipeline))
+test_dataloader = dict(
+    batch_size=test_batch_size_per_gpu,
+    num_workers=test_num_workers,
+    persistent_workers=True,
+    drop_last=False,
+    sampler=dict(type='DefaultSampler', shuffle=False),
+    dataset=dict(
+        type=dataset_type,
+        data_root=data_root,
+        metainfo=METAINFO,
+        ann_file=test_ann_file,
+        data_prefix=dict(img=test_data_prefix),
+        test_mode=True,
+        pipeline=test_pipeline))
+
+val_evaluator = dict(
+    type='CocoMetric',
+    ann_file=data_root + val_ann_file,
+    metric='bbox',
+    format_only=False)
+test_evaluator = dict(
+    type='CocoMetric',
+    ann_file=data_root + test_ann_file,
+    metric='bbox',
+    format_only=False)
+
+# inference on test dataset and
+# format the output results for submission.
+# test_dataloader = dict(
+#     batch_size=1,
+#     num_workers=2,
+#     persistent_workers=True,
+#     drop_last=False,
+#     sampler=dict(type='DefaultSampler', shuffle=False),
+#     dataset=dict(
+#         type=dataset_type,
+#         data_root=data_root,
+#         ann_file=data_root + 'annotations/image_info_test-dev2017.json',
+#         data_prefix=dict(img='test2017/'),
+#         test_mode=True,
+#         pipeline=test_pipeline))
+# test_evaluator = dict(
+#     type='CocoMetric',
+#     metric='bbox',
+#     format_only=True,
+#     ann_file=data_root + 'annotations/image_info_test-dev2017.json',
+#     outfile_prefix='./work_dirs/coco_detection/test')
